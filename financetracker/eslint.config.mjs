dc94da22fca25b63a9762f824559c38d66c553eb
kanav789 <kanavrana91@gmail.com>
2025-07-07 16:25:22 +0530

    off

--- financetracker/eslint.config.mjs
+++ financetracker/eslint.config.mjs
@@ -15,6 +15,8 @@ const eslintConfig = [
     rules: {
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   }),
 ];
