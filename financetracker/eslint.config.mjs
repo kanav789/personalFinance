31aa18e8db41637c80d47480480c2b2e70633e7e
kanav789 <kanavrana91@gmail.com>
2025-07-07 16:36:38 +0530

    change

--- financetracker/eslint.config.mjs
+++ financetracker/eslint.config.mjs
@@ -11,12 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
+    extends: ["next"],
     rules: {
       "react/no-unescaped-entities": "off",
       "@next/next/no-page-custom-font": "off",
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "off",
+      " @typescript-eslint/no-explicit-any": "off",
     },
   }),
 ];
