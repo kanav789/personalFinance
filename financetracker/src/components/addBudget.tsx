e9a618ccec53202cee05c40b2b3e3481971b73ae
kanav789 <kanavrana91@gmail.com>
2025-07-12 14:45:07 +0530

    add budget integration done

---
+++ financetracker/src/components/addBudget.tsx
@@ -0,0 +1,115 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ClipLoader } from "react-spinners";
+import axios from "axios";
+import { useAppDispatch } from "@/lib/hooks";
+import { setData } from "@/redux/feature/dateSlice";
+export default function AddBudget() {
+    const [open, setOpen] = useState(false);
+    const { register, handleSubmit, control, reset, } = useForm();
+    const [Loader, setLoader] = useState(false)
+    const dispatch = useAppDispatch()
+    const onSubmit = async (data: any) => {
+        try {
+            setLoader(true);
+            const body = {
+                ...data,
+            }
+            const response = await axios.post('api/budget/add', body)
+            if (response.status === 200) {
+                const addedbudget = await axios.get('api/budget/list', {
+
+                })
+
+                if (addedbudget.status === 200) {
+
+                    dispatch(setData(addedbudget?.data));
+                    reset();
+                    setOpen(false)
+                }
+            }
+            setLoader(false);
+        } catch (error) {
+            setLoader(false);
+
+            console.error("Error adding budget:", error);
+
+        }
+
+    }
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+
+            <Button
+                className="px-4 py-2 text-sm font-semibold cursor-pointer"
+                onClick={() => setOpen(true)}
+            >
+                Add Budget
+            </Button>
+            <DialogContent className="sm:max-w-[425px]">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <DialogHeader>
+                        <DialogTitle>
+                            {"Add Budget"}
+                        </DialogTitle>
+                        <DialogDescription>
+                            {"Your Budget will expire on the last date of the month."}
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4 py-4">
+                        <div className="grid gap-3">
+                            <Label htmlFor="budgetAmount">Your Budget</Label>
+                            <Input
+                                id="budgetAmount"
+                                type="number"
+                                min="0"
+                                step="0.01"
+                                {...register("budgetAmount", { required: true, maxLength: 6 })}
+                                placeholder="Enter amount"
+                                required
+                            />
+                        </div>
+                        <div className="grid gap-3">
+                            <Label htmlFor="budgetName">Name</Label>
+                            <Input
+                                id="budgetName"
+                                type="text"
+                                {...register("budgetName", { required: true })}
+                                placeholder="Where did you spend?"
+                                required
+                            />
+                        </div>
+
+                    </div>
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button variant="outline" type="button" className="cursor-pointer">
+                                Cancel
+                            </Button>
+                        </DialogClose>
+                        {Loader ? (
+                            <div><ClipLoader /></div>
+                        ) : (
+                            <Button type="submit" className="cursor-pointer">
+                                Add
+                            </Button>
+                        )}
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
+    )
+}
