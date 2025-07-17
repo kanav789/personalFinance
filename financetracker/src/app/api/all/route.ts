efb41bcf16f7ba8896e45d246538b15a22794894
kanav789 <kanavrana91@gmail.com>
2025-07-17 11:49:15 +0530

    changed the fintracker getting all api transactions

--- financetracker/src/app/api/all/route.ts
+++ financetracker/src/app/api/all/route.ts
@@ -1,16 +1,36 @@
-import { NextResponse } from "next/server";
+import { NextResponse,NextRequest } from "next/server";
 import dbConnect from "@/lib/db";
 import Transactions from "@/models/transactionModel";
+import { checkuser } from "@/helpers/middleware/middleware";
 
-export async function GET() {
+export async function POST(request: NextRequest) {
     try {
         dbConnect()
-      const data = await Transactions.find();
+        const body = await request.json();
+        const {email}=body;
+
+        if(!email)
+        {
+            return NextResponse.json({message:"Email is required"},{status:400})
+        }
+        // check user 
+      const user = await checkuser(email)
+      if(!user)
+      {
+        return NextResponse.json({message:"User Not Found"},{status:301})
+      }
+
+                                            
+      const data = await Transactions.find({
+        userId: user._id
+      });
+      console.log("Fetched transactions:", data);
+
       if(!data)
       {
         return NextResponse.json({message:"Transaction Not Found"},{status:301})
       }
- return NextResponse.json({message:"Transaction fetched successfully",data}, {status:200});
+        return NextResponse.json({message:"Transaction fetched successfully",data}, {status:200});
 
     } catch (error) {
         console.log("Error fetching transactions:", error);
